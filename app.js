e5d47be98ce606940359c1b8317487625ca7d48b
nisargpawade <70917826+nisargpawade@users.noreply.github.com>
2021-04-10 20:07:31 +0530

    imported js and started wiring translate btn

---
+++ app.js
@@ -0,0 +1,8 @@
+var translateBtn = document.querySelector("#btn-translate");
+
+// function sendSignal() {
+//     console.log("clicked")
+// }
+alert("it is working")
+console.log("atleast something");
+// translateBtn.addEventListener("click", function sendSignal(){console.log("clicked") } );
